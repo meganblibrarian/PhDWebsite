dea78fd027ee2a59359d946d6b7692be8926b549
meganblibrarian <148710817+meganblibrarian@users.noreply.github.com>
2024-01-23 12:57:56 -0600

    Update basic.cy.js

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -3,9 +3,14 @@ describe('empty spec', () => {
     cy.visit('/')
   })
   it('displays the resources text', () => {
-    cy.get('title')
-    .contains('Welcome');
+    cy.get('h2')
+    .contains('I'm Megan Barnes, a Ph.D. student at the <a href = "lt.unt.edu"> University of North Texas</a>, studying learning technologies. Join me on this journey as I grow as an academic, and share my excitement for technology, research, and the human side of technology with the world.');
   })
+  it('renders the image', () => {
+    cy.get('img')
+    .should('be.visible')
+    .and(($img) => {
+      expect($img[0].naturalWidth).to.be.greaterThan(0);
     })
   })
 })
